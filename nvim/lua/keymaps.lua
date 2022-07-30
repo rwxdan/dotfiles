3f58ed21678f93d288c342db85196d4b66c9e7e2
rwxdan <dan1elcastro@proton.me>
2022-07-30 01:19:48 -0400

    Add keybindings

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,17 +6,23 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 vim.g.mapleader = " "
--- fast save & quit 
+-- fast save & quit using <leader> + {s,x}
 map ('n', '<leader>x', ':x<CR>')
 map ('n', '<leader>s', ':w<CR>')
 map('i', '<leader>s', '<C-c>:w<CR>')
--- quick quit all neovim instances (without save)
+-- quick quit all neovim instances using <leader> + q (without saving)
 map ('n', '<leader>q', ':qa!<CR>')
--- reload configuration without restart nvim
+-- reload configuration without restarting neovim with <leader> + r
 map('n', '<leader>r', ':so %<CR>')
 
--- NERDTree
+-- NERDTree keybindings 
 map ('n', '<leader>n', ':NERDTreeFocus<CR>')
 map ('n', '<C-n>', ':NERDTree<CR>')
 map ('n', '<leader>t', ':NERDTreeToggle<CR>')
 map ('n', '<C-f>', ':NERDTreeFind<CR>')
+
+-- move around splits using Ctrl + {h,j,k,l}
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
