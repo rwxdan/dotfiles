47fad5642d6cdf28509b8dfd07e5746393380953
rwxdan <dan1elcastro@proton.me>
2022-07-30 01:24:49 -0400

    Add EDITOR universal variable

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -1,9 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -gx EDITOR nvim
+
+set -Ux EDITOR nvim
+
 test -f ~/.config/fish/aliases.fish && . ~/.config/fish/aliases.fish
 test -f ~/.config/fish/pm_aliases.fish && . ~/.config/fish/pm_aliases.fish
+
 zoxide init fish | source
 starship init fish | source
 
