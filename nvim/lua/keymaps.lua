c7422ed252f25f0e40a730df26af808df5ca80eb
rwxdan <dan1elcastro@proton.me>
2022-08-05 14:17:38 -0400

    Grammar

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -10,7 +10,7 @@ vim.g.mapleader = " "
 map ('n', '<leader>x', ':x<CR>')
 map ('n', '<leader>s', ':w<CR>')
 map('i', '<leader>s', '<C-c>:w<CR>')
--- quick quit all neovim instances using <leader> + q (without saving)
+-- quick quit all neovim instances using <leader> + q (!without saving)
 map ('n', '<leader>q', ':qa!<CR>')
 -- reload configuration without restarting neovim with <leader> + r
 map('n', '<leader>r', ':so %<CR>')
@@ -21,7 +21,7 @@ map ('n', '<C-n>', ':NERDTree<CR>')
 map ('n', '<leader>t', ':NERDTreeToggle<CR>')
 map ('n', '<C-f>', ':NERDTreeFind<CR>')
 
--- move around splits using Ctrl + {h,j,k,l}
+-- move around split panels using Ctrl + {h,j,k,l}
 map('n', '<C-h>', '<C-w>h')
 map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
