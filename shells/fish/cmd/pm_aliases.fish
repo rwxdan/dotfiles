2ad68f307588e6937a78b3d7822636bbd00ba4f7
rwxdan <dan1elcastro@proton.me>
2022-08-21 01:44:02 -0400

    Fix apt aliases

--- shells/fish/cmd/pm_aliases.fish
+++ shells/fish/cmd/pm_aliases.fish
@@ -9,13 +9,12 @@ if test -x /usr/bin/dnf
 	abbr -a -U -- pfind "dnf -C search"
 
 else if test -x /usr/bin/apt
-	abbr -a -U -- "sudo apt install"
-	abbr -a -U -- "sudo apt update"
-	abbr -a -U -- "sudo apt upgrade"
-	abbr -a -U -- "sudo apt clean"
-  abbr -a -U -- "apt list"
-  abbr -a -U -- "apt-cache show"
-  abbr -a -U -- "sudo apt autoremove"
-  abbr -a -U -- "apt search"
-
+	abbr -a -U -- pin "sudo apt install"
+	abbr -a -U -- pupd "sudo apt update"
+	abbr -a -U -- pupg "sudo apt upgrade"
+	abbr -a -U -- pclean "sudo apt clean"
+  	abbr -a -U -- plist  "apt list"
+  	abbr -a -U -- pinfo "apt-cache show"
+  	abbr -a -U -- prm "sudo apt autoremove"
+  	abbr -a -U -- pfind "apt search"
 end
